bf6dfdba30ea31b3c0c636f4e3c0703aabf2cdf1
Mark Wainwright <wainwrightml@gmail.com>
2021-07-07 12:56:09 +0100

    Removed all connectors from tests

--- Server/LanguageServer.Test/CompletionTest.cs
+++ Server/LanguageServer.Test/CompletionTest.cs
@@ -3,8 +3,6 @@ using System.Reflection;
 using FluentAssertions;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using Reductech.EDR.ConnectorManagement.Base;
-using Reductech.EDR.Connectors.FileSystem;
-using Reductech.EDR.Connectors.StructuredData;
 using Reductech.EDR.Core.Internal;
 using Xunit;
 using static LanguageServer.Test.TestHelpers;
@@ -13,39 +11,24 @@ namespace LanguageServer.Test
 {
     public class CompletionTest
     {
-        public const string LongText = @"FileRead    'artwork_data.csv'
-| FromCSV    
-| ArrayFilter ((from <entity> 'artist') == 'Blake, Robert')
-| EntityMap (in <entity> 'artist' (StringToCase (from <entity> 'artist') TextCase.Upper ))
-| EntityMapProperties (artist: 'Artist Name' artistId: 'ArtistId')
-| ArraySort (from <entity> 'year')
-| ArrayDistinct (from <entity> 'id')
-| ToJson
-| FileWrite 'Artwork_Data.json'
-";
+        
 
         public const string ErrorText = @"- FileRead 'artwork_data.csv'
 - 0.1.2.3";
 
         [Theory]
-        [InlineData("Print 123", 0, 1, "Print")]
-        [InlineData(LongText, 0, 9, "Path")]
-        [InlineData("- FileRead  ", 0, 11, "Path")]
-        [InlineData("FileRead  ", 0, 9, "Path")]
-        [InlineData("FileRead P", 0, 10, "Path")]
-        [InlineData("FileRead\r\nP", 1, 0, "Path")]
-        [InlineData("- FileRead\r\nP", 1, 0, "Path")]
-        [InlineData("- FileRead P", 0, 12, "Path")]
-        [InlineData(LongText, 1, 3, "FromCSV")]
+        [InlineData("Print 123", 0, 6, "Value")]
+        [InlineData("- Print  ", 0, 11, "Value")]
+        [InlineData("Print  ", 0, 9, "Value")]
+        [InlineData("Print P", 0, 8, "Value")]
+        [InlineData("Print\r\nP", 1, 0, "Value")]
+        [InlineData("- Print\r\nP", 1, 0, "Value")]
+        [InlineData("- Print P", 0, 8, "Value")]
+        [InlineData(LongText, 1, 3, "ArrayFilter")]
         public void ShouldGiveCorrectCompletion(string text, int line, int character, string? expectedLabel)
         {
-            var fsAssembly = Assembly.GetAssembly(typeof(FileRead))!;
-            var sdAssembly = Assembly.GetAssembly(typeof(ToJson))!;
 
-            var fsConnectorData = new ConnectorData(ConnectorSettings.DefaultForAssembly(fsAssembly), fsAssembly);
-            var sdConnectorData = new ConnectorData(ConnectorSettings.DefaultForAssembly(sdAssembly), sdAssembly);
-
-            var sfs = StepFactoryStore.Create(fsConnectorData, sdConnectorData);
+            var sfs = StepFactoryStore.Create();
             var document = new SCLDocument(text, DefaultUri);
 
             var position = new Position(line, character);
--- Server/LanguageServer.Test/HoverTest.cs
+++ Server/LanguageServer.Test/HoverTest.cs
@@ -1,10 +1,6 @@
 using System.Linq;
-using System.Reflection;
 using FluentAssertions;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using Reductech.EDR.ConnectorManagement.Base;
-using Reductech.EDR.Connectors.FileSystem;
-using Reductech.EDR.Connectors.StructuredData;
 using Reductech.EDR.Core.Internal;
 using Xunit;
 using static LanguageServer.Test.TestHelpers;
@@ -13,16 +9,6 @@ namespace LanguageServer.Test
 {
     public class HoverTest
     {
-        public const string LongText = @"FileRead 'artwork_data.csv'
-| FromCSV
-| ArrayFilter ((from <entity> 'artist') == 'Blake, Robert')
-| EntityMap (in <entity> 'artist' (StringToCase (from <entity> 'artist') TextCase.Upper ))
-| EntityMapProperties (artist: 'Artist Name' artistId: 'ArtistId')
-| ArraySort (from <entity> 'year')
-| ArrayDistinct (from <entity> 'id')
-| ToJson
-| FileWrite 'Artwork_Data.json'
-";
 
         public const string ErrorText = @"- FileRead 'artwork_data.csv'
 - 0.1.2.3";
@@ -31,19 +17,12 @@ namespace LanguageServer.Test
         [InlineData("Print 123", 0, 1, "`Print`", "`Unit`", "Prints a value to the console.")]
         [InlineData("Print 123", 0, 8, "`123`",  "`Integer`")]
         //[InlineData(LongText, 0, 1, "Reads text from a file.")] //doesn't work
-        [InlineData(LongText, 0, 12, "`'artwork_data.csv'`", "`String`")]
-        [InlineData(LongText, 1, 3,
-            "`FromCSV`", "`Array<T>`", "Extracts entities from a CSV file.\nThe same as FromConcordance but with different default values.")]
+        [InlineData(LongText, 0, 12, "`'Blake, Robert'`", "`String`")]
+        [InlineData(LongText, 1, 3, "`ArrayFilter`", "`Array<T>`", "Filter an array according to a function.")]
         //[InlineData(ErrorText, 0, 1, "Reads text from a file.")]
         public void ShouldGiveCorrectHover(string text, int line, int character, params string[] expectedHovers)
         {
-            var fsAssembly = Assembly.GetAssembly(typeof(FileRead))!;
-            var sdAssembly = Assembly.GetAssembly(typeof(ToJson))!;
-
-            var fsConnectorData = new ConnectorData(ConnectorSettings.DefaultForAssembly(fsAssembly), fsAssembly);
-            var sdConnectorData = new ConnectorData(ConnectorSettings.DefaultForAssembly(sdAssembly), sdAssembly);
-
-            var sfs = StepFactoryStore.Create(fsConnectorData, sdConnectorData);
+            var sfs = StepFactoryStore.Create();
 
             var document = new SCLDocument(text, DefaultUri);
 
