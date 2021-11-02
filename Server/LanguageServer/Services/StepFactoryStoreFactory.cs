5d61fce68119c4dc9e1ee694a164f5f887ee9103
Mark Wainwright <wainwrightml@gmail.com>
2021-11-02 16:23:04 +0000

    Updated StepFactoryStore

--- Server/LanguageServer/Services/StepFactoryStoreFactory.cs
+++ Server/LanguageServer/Services/StepFactoryStoreFactory.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.IO.Abstractions;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using Reductech.EDR.ConnectorManagement;
 using Reductech.EDR.ConnectorManagement.Base;
+using Reductech.EDR.Core.Abstractions;
 using Reductech.EDR.Core.Connectors;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Logging;
 
 namespace LanguageServer.Services
 {
@@ -82,20 +85,18 @@ namespace LanguageServer.Services
                             fileSystem
                         );
 
-                    StepFactoryStore sfs;
+                    var sfsResult = await 
+                        connectorManager.GetStepFactoryStoreAsync(ExternalContext.Default, CancellationToken.None);
 
-                    try
+                    if (sfsResult.IsFailure)
                     {
-                        sfs = await connectorManager.GetStepFactoryStoreAsync();
+                        logger.LogError(sfsResult.Error.AsString);
+                        return StepFactoryStore.Create();
                     }
-                    catch (Exception e)
-                    {
-                        logger.LogError(e, e.Message);
-                        sfs = StepFactoryStore.Create();
-                    }
+                        
                     
 
-                    return sfs;
+                    return sfsResult.Value;
                 }, optionsMonitor
             );
 
